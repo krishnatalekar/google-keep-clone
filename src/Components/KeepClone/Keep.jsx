057539435248411ff2dc8bf85692eae69f81f52b
Krishna Talekar <krishnatalekar@Krishnas-MacBook-Air.local>
2024-05-15 22:33:40 +0530

    first commit

---
+++ src/Components/KeepClone/Keep.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "./Keep.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+const Keep = () => {
+  const [notes, setNotes] = useState([]);
+
+  const addNote = (note) => {
+    setNotes((prevNotes) => {
+      return [...prevNotes, note];
+    });
+  };
+
+  const deleteNote = (id) => {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  };
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+};
+
+export default Keep;
